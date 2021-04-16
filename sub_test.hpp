750e9cf1fffc57960fced66f504f1e890d59ec8d
alice-thaitea <athai026@ucr.edu>
2021-04-16 15:23:43 -0700

    Added 10-5-5 unit test

---
+++ sub_test.hpp
@@ -0,0 +1,17 @@
+#ifndef __SUB_TEST_HPP__
+#define __SUB_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "sub.hpp"
+#include "mocks/mock.hpp"
+
+TEST(SubTest, SubEval_Ten_minus_Five) {
+    Base* op1 = new mockOpTen();
+    Base* op2 = new mockOpFive();
+    Sub* test = new Sub(op1, op2);
+    EXPECT_EQ(test->evaluate(), 5);
+}
+
+#endif //__SUB_TEST_HPP__
+
