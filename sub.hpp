abd382736218279f1c748855da329ccbe62d267b
alice-thaitea <athai026@ucr.edu>
2021-04-16 15:07:54 -0700

    Updated sub.hpp

--- sub.hpp
+++ sub.hpp
@@ -8,20 +8,22 @@ class Sub : public Base {
         Sub(Base* left, Base* right) : Base() {
             l_value = left->evaluate();
             r_value = right->evaluate();
+            l_string = left->stringify();
+            r_string = right->stringify();
         }
         virtual double evaluate() {
             difference = l_value = r_value;
             return difference;
         }
         virtual std::string stringify() {
-            l_string = left->stringify();
-            r_string = right->stringify();
             return ("(" + l_string + "-" + r_string + ")");
         }
 
     public: 
         double l_value, r_value, difference;
         std::string l_string, r_string;
+        Base* left;
+        Base* right;
 };
 
 #endif //__SUB_HPP__
