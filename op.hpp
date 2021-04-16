39e8800c914b6aff1c3f591865a8841174052d6d
alice-thaitea <athai026@ucr.edu>
2021-04-15 18:15:56 -0700

    update op.hpp

--- op.hpp
+++ op.hpp
@@ -1,13 +1,21 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
 
+#include "iostream"
+#include <iomanip>
 #include "base.hpp"
 
 class Op : public Base {
     public:
-        Op(double value) : Base() { }
-        virtual double evaluate() { return 0.0; }
-        virtual std::string stringify() { return ""; }
+        Op(double value) : Base(), value(value) { }
+        virtual double evaluate() { return value; }
+        virtual std::string stringify() {
+            ostringstream  ss;
+            ss << setprecision(8) << noshowpoint << value;
+            return ss.str();
+        }
+    private: 
+        double value;
 };
 
 #endif //__OP_HPP__
