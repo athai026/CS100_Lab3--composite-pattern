e62cac1e81db8d3bcc9b66bc70b7840ba0240a84
alice-thaitea <athai026@ucr.edu>
2021-04-16 13:13:40 -0700

    Fixed op.hpp

--- op.hpp
+++ op.hpp
@@ -1,17 +1,19 @@
 #ifndef __OP_HPP__
 #define __OP_HPP__
 
-#include "iostream"
+#include <iostream>
 #include <iomanip>
+#include <cstdlib>
+#include <sstream>
 #include "base.hpp"
 
 class Op : public Base {
     public:
-        Op(double value) : Base(), value(value) { }
+        Op(double value) : Base() ,value(value) {}
         virtual double evaluate() { return value; }
-        virtual std::string stringify() {
-            ostringstream  ss;
-            ss << setprecision(8) << noshowpoint << value;
+        virtual std::string stringify() { 
+            std::ostringstream ss;
+            ss << std::setprecision(8) << std::noshowpoint << value;
             return ss.str();
         }
     private: 
