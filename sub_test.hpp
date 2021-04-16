3c915b90b8e33cb5d8d709a5ad5ddd5f60f12bbc
alice-thaitea <athai026@ucr.edu>
2021-04-16 15:35:56 -0700

    Added (10-5) stringify() unit test

--- sub_test.hpp
+++ sub_test.hpp
@@ -20,6 +20,14 @@ TEST(SubTest, SubEval_Five_minus_Ten) {
     EXPECT_EQ(test->evaluate(), -5);
 }
 
+TEST(SubTest, SubString_Ten_minus_Five) {
+    Base* op1 = new mockOpTen();
+    Base* op2 = new mockOpFive();
+    Sub* test = new Sub(op1, op2);
+    std::string result = "(10-5)";
+    EXPECT_EQ(test->stringify(), result);
+}
+
 
 #endif //__SUB_TEST_HPP__
 
