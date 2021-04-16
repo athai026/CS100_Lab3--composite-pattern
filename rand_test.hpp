3c29c97df80df81aeb0d58a2d892133cc4621a4a
alice-thaitea <athai026@ucr.edu>
2021-04-16 14:02:11 -0700

    Added rand()>=0 unit test

---
+++ rand_test.hpp
@@ -0,0 +1,13 @@
+#ifndef __RAND_TEST_HPP__
+#define __RAND_TEST_HPP__
+
+#include "gtest/gtest.h"
+
+#include "rand.hpp"
+
+TEST(RandTest, RandEvaluateGE0) {
+    Rand* test = new Rand();
+    EXPECT_GE(test->evaluate(), 0);
+}
+
+#endif //__RAND_TEST_HPP__
