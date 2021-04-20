24ac9fbf7f24eb7bde9a75c8cb495dc43a5f2c43
AEssaian740 <aessaian101@gmail.com>
2021-04-20 11:40:37 -0700

    Added (-7*10) evaluate() unit test

--- mult_test.hpp
+++ mult_test.hpp
@@ -13,4 +13,11 @@ TEST(MultTest, MultEval_Ten_times_NegSeven) {
     EXPECT_EQ(test->evaluate(), -70);
 }
 
+TEST(MultTest, MultEval_NegSeven_times_Ten) {
+    Base* op1 = new mockOpNegSeven();
+    Base* op2 = new mockOpTen();
+    Mult* test = new Mult(op1, op2);
+    EXPECT_EQ(test->evaluate(), -70);
+}
+
 #endif
