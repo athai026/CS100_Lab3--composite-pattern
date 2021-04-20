238dbf4d1bbacf29fd9536f378e2ca6b77ff8ee0
AEssaian740 <aessaian101@gmail.com>
2021-04-20 11:30:05 -0700

    Added (10+-7) nad (-7+10) stringify() unit test

--- add_test.hpp
+++ add_test.hpp
@@ -20,5 +20,21 @@ TEST(AddTest, AddEval_NegSeven_plus_Ten) {
     EXPECT_EQ(test->evaluate(), 3);
 }
 
+TEST(AddTest, AddString_Ten_plus_NegSeven) {
+    Base* op1 = new mockOpTen();
+    Base* op2 = new mockOpNegSeven();
+    Add* test = new Add(op1, op2);
+    std::string result = "(10+-7)";
+    EXPECT_EQ(test->stringify(), result);
+}
+
+TEST(AddTest, AddString_NegSeven_plus_Ten) {
+    Base* op1 = new mockOpNegSeven();
+    Base* op2 = new mockOpTen();
+    Add* test = new Add(op1, op2);
+    std::string result = "(-7+10)";
+    EXPECT_EQ(test->stringify(), result);
+}
+
 #endif //__ADD_TEST_HPP__
 
