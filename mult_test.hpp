a3fdd427a90d9f739772d081b44a24fdccc66b11
AEssaian740 <aessaian101@gmail.com>
2021-04-20 11:44:52 -0700

    Added (mockAdd()*mockPow()) evaluate() and stringify() unit test

--- mult_test.hpp
+++ mult_test.hpp
@@ -51,4 +51,19 @@ TEST(MultTest, MultString_Sub_times_Five) {
     EXPECT_EQ(test->stringify(), result);
 }
 
+TEST(MultTest, MultEval_Add_times_Pow) {
+    Base* op1 = new mockAdd();
+    Base* op2 = new mockPow();
+    Mult* test = new Mult(op1, op2);
+    EXPECT_EQ(test->evaluate(), 2511);
+}
+
+TEST(MultTest, MultString_Add_times_Pow) {
+    Base* op1 = new mockAdd();
+    Base* op2 = new mockPow();
+    Mult* test = new Mult(op1, op2);
+    std::string result = "((13+18)*(3**4))";
+    EXPECT_EQ(test->stringify(), result);
+}
+
 #endif
